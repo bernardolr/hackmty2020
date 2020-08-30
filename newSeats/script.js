e956b7bc56a17041be20a8a8a79ffb2f648ffec7
Bernardo <bernardolr17@gmail.com>
2020-08-29 21:55:04 -0500

    almost?

--- newSeats/script.js
+++ newSeats/script.js
@@ -6,6 +6,8 @@ const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelector = document.getElementById("movie");
 
+populateUI();
+
 let ticketPrice = +movieSelector.value; //turning the string value into an int
 
 //Save selected movie index and price
@@ -37,6 +39,29 @@ function updateSelectedCount(){
 }
 
 
+//Get data from localstorage and populate UI
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats')
+    );
+
+    if(selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if (selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected');
+                
+            }
+
+        });
+    }
+
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+    
+    if (selectedMovieIndex !== null) {
+        movieSelect.selectedIndex == selectedMovieIndex;
+        
+    }
+}
+
 // Movie select event
 movieSelector.addEventListener('change', e=>{
     ticketPrice = +e.target.value;
@@ -57,3 +82,7 @@ container.addEventListener('click', e=> {
         updateSelectedCount();
     }
 });
+
+//Initial count and total set
+
+updateSelectedCount();
